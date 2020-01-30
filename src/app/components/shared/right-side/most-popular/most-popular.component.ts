472b61df2120cb74733d823d42dae0ae3bd907af
alparslanylmaz <alparslanylmaz@gmail.com>
2020-01-30 15:10:00 +0300

    most popular component designed

--- src/app/components/shared/right-side/most-popular/most-popular.component.ts
+++ src/app/components/shared/right-side/most-popular/most-popular.component.ts
@@ -12,5 +12,26 @@ export class MostPopularComponent implements OnInit {
   ngOnInit() {
   }
 
-  
+  mostPopularCards = [
+    {
+      id: '1',
+      categoryName: 'Fashion',
+      title: 'First of all that is a title',
+      pictureUrl: 'https://images.pexels.com/photos/3363331/pexels-photo-3363331.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    },
+    {
+      id: '2',
+      categoryName: 'Travel',
+      title: 'First of all that is a title',
+      pictureUrl: 'https://images.pexels.com/photos/3363331/pexels-photo-3363331.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    
+    },
+    {
+      id: '3',
+      categoryName: 'Science',
+      title: 'First of all that is a title',
+      pictureUrl: 'https://images.pexels.com/photos/3363331/pexels-photo-3363331.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    
+    }
+  ];
 }
